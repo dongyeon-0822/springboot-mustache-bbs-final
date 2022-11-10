62909873c733c240a971ca1a6f5e09c168db3387
DongYeon Kang <myjjue00@gmail.com>
2022-11-10 13:53:22 +0900

    Add editPage, updateArticle (Update)

--- src/main/java/com/mustache/springbootmustache3/controller/ArticleController.java
+++ src/main/java/com/mustache/springbootmustache3/controller/ArticleController.java
@@ -24,26 +24,31 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
-    @GetMapping("/new")
+    @GetMapping("/new") // 새로운 게시글을 작성하는 페이지
     public String createArticlePage() {
         return "articles/new";
     }
 
-    @GetMapping("/list")
+    @GetMapping("") // index : 게시판 메인화면
+    public String index() {
+        return "redirect:/articles/list";
+    }
+
+    @GetMapping("/list") // 게시판 페이지(findAll)
     public String listPage(Model model) {
         List<Article> articles = articleRepository.findAll();
         model.addAttribute("articles", articles);
         return "articles/list";
     }
 
-    @PostMapping("")
+    @PostMapping("") // 작성한 게시글을 DB에 저장(add)
     public String createArticle(ArticleDto articleDto) {
         log.info(articleDto.getTitle());
         Article savedArticle=articleRepository.save(articleDto.toEntity());
         return String.format("redirect:/articles/%d",savedArticle.getId()) ;
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/{id}") // 하나의 게시글을 보여주는 페이지 (select)
     public String showSingle(@PathVariable Long id, Model model) {
         Optional<Article> optArticle = articleRepository.findById(id);
 
@@ -53,5 +58,25 @@ public class ArticleController {
         } else {
             return "articles/error";
         }
+    }
+
+    @GetMapping(value = "/{id}/edit") // 게시글을 수정하는 페이지
+    public String editPage(@PathVariable Long id, Model model) {
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+
+        if(!optionalArticle.isEmpty()){
+            model.addAttribute("article", optionalArticle.get());
+            return "articles/edit";
+        } else {
+            model.addAttribute("message", String.format("%d가 없습니다.", id));
+            return "articles/error";
+        }
+    }
+    @PostMapping(value = "/{id}/update") // 수정한 게시글을 다시 DB에 저장(update)
+    public String updateArticle(@PathVariable Long id, ArticleDto articleDto, Model model) {
+        log.info("title:{} content:{}", articleDto.getTitle(), articleDto.getContent());
+        Article article = articleRepository.save(articleDto.toEntity());
+        model.addAttribute("article", article);
+        return String.format("redirect:/articles/%d", article.getId());
     }
 }
