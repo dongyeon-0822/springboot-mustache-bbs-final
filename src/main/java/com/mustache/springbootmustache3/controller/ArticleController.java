c1bcd0641a4e30079eea53f0ffd1cee4371a4841
DongYeon Kang <myjjue00@gmail.com>
2022-11-10 11:32:34 +0900

    Create ArticleController.java - Add createArticlePage()

---
+++ src/main/java/com/mustache/springbootmustache3/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package com.mustache.springbootmustache3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/articles")
+public class ArticleController {
+    @GetMapping
+    public String createArticlePage() {
+        return "new";
+    }
+}
