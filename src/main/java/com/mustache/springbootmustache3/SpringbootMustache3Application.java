8f8825eebee2aaa9782292acebdd6802ccc30bac
DongYeon Kang <myjjue00@gmail.com>
2022-11-10 11:10:44 +0900

    first commit

---
+++ src/main/java/com/mustache/springbootmustache3/SpringbootMustache3Application.java
@@ -0,0 +1,13 @@
+package com.mustache.springbootmustache3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustache3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMustache3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustache/springbootmustache3/SpringbootMustache3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.springbootmustache3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMustache3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
