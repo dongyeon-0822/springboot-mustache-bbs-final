d061bbbe445d5c1abc166c7211c0e8262e100749
DongYeon Kang <myjjue00@gmail.com>
2022-11-10 11:33:26 +0900

    Create Article Entity

---
+++ src/main/java/com/mustache/springbootmustache3/domain/entity/Article.java
@@ -0,0 +1,25 @@
+package com.mustache.springbootmustache3.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Article {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
