1de08157b8973c2518969ff467ac7c4ecd112c80
DongYeon Kang <myjjue00@gmail.com>
2022-11-14 13:33:26 +0900

    Add query - '경기도 수원시'에 있는 보건진료소, 보건지소, 보건소 모두 찾는 sql문 작성

--- src/main/resources/sql/Hospital.sql
+++ src/main/resources/sql/Hospital.sql
@@ -1,2 +1,3 @@
 select * from nation_wide_hospitals where road_name_address like '경기도 수원시%';
 select * from nation_wide_hospitals where road_name_address like '경기도 수원시%' and hospital_name like '%피부%';
+select * from nation_wide_hospitals where road_name_address like '경기도 수원시%' and business_type_name like '%보건%';
