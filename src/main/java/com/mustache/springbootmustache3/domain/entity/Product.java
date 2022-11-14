5d1f23e34be6d6163566801818d86646f6c18071
DongYeon Kang <myjjue00@gmail.com>
2022-11-14 11:28:15 +0900

    Create Product entity

---
+++ src/main/java/com/mustache/springbootmustache3/domain/entity/Product.java
@@ -0,0 +1,24 @@
+package com.mustache.springbootmustache3.domain.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product") //생략가능
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false) //not null
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer Stock;
+
+    private LocalDateTime createAt;
+    private LocalDateTime updatedAt;
+}
