34c55153a39058a7db4a4e1ecdfe24c7a2b55c20
DongYeon Kang <myjjue00@gmail.com>
2022-11-14 14:07:39 +0900

    Add findByBusinessTypeNameIn query

--- src/main/resources/sql/Hospital.sql
+++ src/main/resources/sql/Hospital.sql
@@ -1,3 +1,8 @@
 select * from nation_wide_hospitals where road_name_address like '경기도 수원시%';
 select * from nation_wide_hospitals where road_name_address like '경기도 수원시%' and hospital_name like '%피부%';
 select * from nation_wide_hospitals where road_name_address like '경기도 수원시%' and business_type_name like '%보건%';
+SELECT business_type_name, hospital_name, road_name_address
+FROM nation_wide_hospitals
+where business_type_name in ('보건소', '보건지소');
+
+
