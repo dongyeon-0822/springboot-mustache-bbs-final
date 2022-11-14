0a36e320a7257fc4a8e5e295b045efc39d20dcb5
DongYeon Kang <myjjue00@gmail.com>
2022-11-14 14:08:39 +0900

    Create findByBusinessTypeNameIn test

--- src/test/java/com/mustache/springbootmustache3/repository/HospitalRepositoryTest.java
+++ src/test/java/com/mustache/springbootmustache3/repository/HospitalRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,6 +16,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class HospitalRepositoryTest {
     @Autowired
     HospitalRepository hospitalRepository;
+
+    @Test
+    @DisplayName("BusinessTypeName을 잘 가져오는지")
+    void findByBusinessTypeNameIn() {
+        List<String> inClues = new ArrayList<>();
+        inClues.add("보건소");
+        inClues.add("보건지소");
+        inClues.add("보건진료소");
+        List<Hospital> hospitals = hospitalRepository.findByBusinessTypeNameIn(inClues);
+        for (var hospital :
+                hospitals) {
+            System.out.println(hospital.getHospitalName());
+        }
+    }
+
+
 
     @Test
     @DisplayName("DB에서 값을 잘 가져오는지 확인")
