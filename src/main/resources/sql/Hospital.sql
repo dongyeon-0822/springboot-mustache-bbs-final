949f8e91c3b1c9810f1f6eca3d9fd6e8c7b83692
DongYeon Kang <myjjue00@gmail.com>
2022-11-14 13:28:33 +0900

    Add query - '경기도 수원시'에 있는 피부과의원 sql문 작성

--- src/main/resources/sql/Hospital.sql
+++ src/main/resources/sql/Hospital.sql
@@ -1,1 +1,2 @@
 select * from nation_wide_hospitals where road_name_address like '경기도 수원시%';
+select * from nation_wide_hospitals where road_name_address like '경기도 수원시%' and hospital_name like '%피부%';
