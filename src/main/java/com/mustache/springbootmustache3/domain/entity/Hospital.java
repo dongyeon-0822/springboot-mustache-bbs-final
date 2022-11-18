df504a20227738ca7a0fb70ca3a35982a8e27f94
DongYeon Kang <myjjue00@gmail.com>
2022-11-18 13:10:44 +0900

    feat: HospitalServiceTest 추가
    - Add Builder in Hospital.java

--- src/main/java/com/mustache/springbootmustache3/domain/entity/Hospital.java
+++ src/main/java/com/mustache/springbootmustache3/domain/entity/Hospital.java
@@ -1,10 +1,7 @@
 package com.mustache.springbootmustache3.domain.entity;
 
 import com.mustache.springbootmustache3.domain.dto.HospitalResponse;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,6 +14,7 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Hospital {
     @Id
     private Integer id;
